43cf2f09fd63e86bfa03c606abfe6c3ba6b64b88
Maxim Vertyachkin <maksimvertyachkin@gmail.com>
2021-11-18 15:32:59 +0300

    Первый тест Dom дз

--- projects/dom/index.js
+++ projects/dom/index.js
@@ -10,7 +10,11 @@
  Пример:
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
-function createDivWithText(text) {}
+function createDivWithText(text) {
+  let div = document.createElement('div');
+  div.textContent = text;
+  return div
+}
 
 /*
  Задание 2:
@@ -20,7 +24,10 @@ function createDivWithText(text) {}
  Пример:
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
-function prepend(what, where) {}
+function prepend(what, where) {
+  let prependFn = where.prepend(what);
+  return prependFn;
+}
 
 /*
  Задание 3:
@@ -41,7 +48,16 @@ function prepend(what, where) {}
 
    findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
  */
-function findAllPSiblings(where) {}
+function findAllPSiblings(where) {
+  let w = where.children
+  let siblings = [];
+  for(let child of w){
+    if(child.nextElementSiblings.nodeName == 'p'){
+      siblings.push(child)
+    }
+  }
+  return siblings;
+}
 
 /*
  Задание 4:
@@ -63,10 +79,10 @@ function findAllPSiblings(where) {}
 function findError(where) {
   const result = [];
 
-  for (const child of where.childNodes) {
+  for (const child of where.children) {
     result.push(child.textContent);
   }
-
+   
   return result;
 }
 
@@ -82,7 +98,13 @@ function findError(where) {
    После выполнения функции, дерево <div></div>привет<p></p>loftchool!!!
    должно быть преобразовано в <div></div><p></p>
  */
-function deleteTextNodes(where) {}
+function deleteTextNodes(where) {
+  for(let el of where.childNodes){
+    if(el.nodeType === 3){
+      where.childNodes.removeChild(el)
+    }
+  }
+}
 
 /*
  Задание 6:
