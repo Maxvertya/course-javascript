695899bb958607f9d004a6febea35e6c0037a125
Maxim Vertyachkin <maksimvertyachkin@gmail.com>
2021-11-27 17:00:58 +0300

    DOM events home work

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -9,7 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
-  target.addEventListener('eventName', fn)
+  target.addEventListener(eventName, fn)
 }
 
 /*
@@ -21,7 +21,7 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
-  target.removeEventLitener('eventName', fn)
+  target.removeEventLitener(eventName, fn)
 }
 
 /*
@@ -33,7 +33,7 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
-  target.addEventListener('eventName', e =>{
+  target.addEventListener(eventName, e =>{
     e.preventDefault();
   })
 }
@@ -83,7 +83,7 @@ function once(target, fn) {
   target.addEventListener('click', ()=>{
     if(!current){
       fn();
-      already= true
+      already = true
     }
   })
 }
